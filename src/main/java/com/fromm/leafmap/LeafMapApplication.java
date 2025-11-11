20be3afed104c6bd13973940b624034448c3d332
jeondain <dain.jeon03@gmail.com>
2025-11-11 12:30:14 +0900

    Initial commit

---
+++ src/main/java/com/fromm/leafmap/LeafMapApplication.java
@@ -0,0 +1,13 @@
+package com.fromm.leafmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeafMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeafMapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fromm/leafmap/LeafMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fromm.leafmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeafMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
