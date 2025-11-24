8c9d7f2fd4a66695039d9f864732a1448cee70ed
jeondain <dain.jeon03@gmail.com>
2025-11-24 19:02:23 +0900

    [refactor #54] 홈 화면 조회 API 응답 수정

--- src/main/java/com/fromm/leafmap/domain/post/service/PostServiceImpl.java
+++ src/main/java/com/fromm/leafmap/domain/post/service/PostServiceImpl.java
@@ -34,7 +34,11 @@ public class PostServiceImpl implements PostService {
     @Override
     @Transactional
     public PostResponseDTO.RestaurantPostListResultDTO getRestaurantPostList(Member member) {
-        List<Post> posts = postRepository.findTop10ByBoardTypeAndIsPublicTrueOrderByCreatedAtDesc(BoardType.RESTAURANT);
+
+        List<Post> posts = postRepository.findRandom10RestaurantPosts(
+                BoardType.RESTAURANT,
+                PageRequest.of(0, 10)
+        );
 
         List<PostResponseDTO.RestaurantPostPreviewDTO> previewList = posts.stream()
                 .map(post -> PostResponseDTO.RestaurantPostPreviewDTO.builder()
@@ -42,7 +46,6 @@ public class PostServiceImpl implements PostService {
                         .imageUrl(post.getImageUrl())
                         .build())
                 .toList();
-
 
         return PostResponseDTO.RestaurantPostListResultDTO.builder()
                 .posts(previewList)
